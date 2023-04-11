64ee7e80ca8290e49554e99fd820652235e7eedf
Riya Goyal <113978413+riyagoel930@users.noreply.github.com>
2023-04-11 21:17:30 +0530

    Create findTable.java

---
+++ findTable.java
@@ -0,0 +1,19 @@
+package Oops;
+import java.util.Scanner;
+public class operator {
+	public static void main(String[] args) {
+		   // TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any number: ");
+		 int num = sc.nextInt();
+		findtable(num);
+	}
+	private static void findtable(int num) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<11;i++)
+		{
+			int prod= num * i;
+			System.out.println("Table:  " + prod);
+		}
+	}
+	}
